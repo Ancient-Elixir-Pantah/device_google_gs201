d8405f5fdb1e69cfe6c0b0f3d6ca5beda7288e59
Pyrtle93 <xxpyrtlexx93@gmail.com>
2023-11-30 00:38:28 +0530

    gs201: add nga prop & some clean-ups

--- ancient_common.mk
+++ ancient_common.mk
@@ -9,3 +9,6 @@ DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-ancient
 # AiAi Config
 PRODUCT_COPY_FILES += \
     device/google/gs201/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
+
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
